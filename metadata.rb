20ebd8f57b496b3c384b4bfbe3443981a5aa3a9b
Tim Smith <tsmith@chef.io>
2017-07-25 20:44:22 -0700

    Add new chef_version, issues_url, and source_url metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,7 @@ description       'Creates rpm package for fcgiwrap.'
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 license           'Apache-2.0'
 version           '2.0.1'
+chef_version      '>= 12.1'
 
 #------------------------------------------------------- cookbook dependencies
 depends           'yum-epel', '>= 1.0.2'
@@ -19,3 +20,7 @@ supports          'amazon'
 supports          'oracle'
 supports          'redhat'
 supports          'scientific'
+
+#-------------------------------------------------------- supermarket metadata
+source_url       'https://github.com/4-20ma/cookbook-fcgiwrap_rpm'
+issues_url       'https://github.com/4-20ma/cookbook-fcgiwrap_rpm/issues'
